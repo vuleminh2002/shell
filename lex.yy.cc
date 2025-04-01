51f452ba05267851cae846c6970132fc23ea49b6
Minh Le Vu <vu25@data.cs.purdue.edu>
2025-04-01 16:39:33 -0400

    Commit

--- lex.yy.cc
+++ lex.yy.cc
@@ -1063,7 +1063,7 @@ YY_RULE_SETUP
     // Read from pout[0]
     char buffer[4096] = {0};
     int n = read(pipeOut[0], buffer, sizeof(buffer));
-    printf("%c", *buffer);
+    fprintf(stderr, "Subshell output inserted: [%s]\n", buffer);
     close(pipeOut[0]);
 
     // Push the output back into the lexer input buffer
